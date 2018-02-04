312ade2308fc350001fe602b836da94155b71e31
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-04 14:31:09 +0000

    Deploy NekiCat/fanfiction-enhancements to github.com/NekiCat/fanfiction-enhancements.git:gh-pages

---
+++ latest/fanfiction-enhancements.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         FanFiction Enhancements
+// @namespace    https://tiger.rocks/
+// @version      0.1.0
+// @description  FanFiction.net Enhancements
+// @author       Arne 'TigeR' Linck
+// @copyright    2018, Arne 'TigeR' Linck
+// @license      MIT, https://github.com/NekiCat/fanfiction-enhancements/blob/master/LICENSE
+// @homepageURL  https://github.com/NekiCat/fanfiction-enhancements
+// @supportURL   https://github.com/NekiCat/fanfiction-enhancements/issues
+// @updateURL    https://raw.githubusercontent.com/NekiCat/fanfiction-enhancements/master/fanfiction-enhancements.meta.js
+// @downloadURL  https://raw.githubusercontent.com/NekiCat/fanfiction-enhancements/master/fanfiction-enhancements.user.js
+// @require      https://raw.githubusercontent.com/taylorhakes/promise-polyfill/master/promise.min.js
+// @match        *://www.fanfiction.net/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
+
+(function () {
+'use strict';
+
+console.info("Hello World!");
+
+}());
