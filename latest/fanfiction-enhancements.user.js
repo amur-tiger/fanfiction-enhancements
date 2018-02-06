e501d1390f83ea79283db72528b53251697f2162
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-02-06 21:51:59 +0000

    Deploy NekiCat/fanfiction-enhancements to github.com/NekiCat/fanfiction-enhancements.git:gh-pages

--- latest/fanfiction-enhancements.user.js
+++ latest/fanfiction-enhancements.user.js
@@ -1,19 +1,18 @@
 // ==UserScript==
 // @name         FanFiction Enhancements
 // @namespace    https://tiger.rocks/
-// @version      0.1.0
+// @version      0.1.0+7.33acca7
 // @description  FanFiction.net Enhancements
 // @author       Arne 'TigeR' Linck
 // @copyright    2018, Arne 'TigeR' Linck
 // @license      MIT, https://github.com/NekiCat/fanfiction-enhancements/blob/master/LICENSE
 // @homepageURL  https://github.com/NekiCat/fanfiction-enhancements
 // @supportURL   https://github.com/NekiCat/fanfiction-enhancements/issues
-// @updateURL    https://raw.githubusercontent.com/NekiCat/fanfiction-enhancements/master/fanfiction-enhancements.meta.js
-// @downloadURL  https://raw.githubusercontent.com/NekiCat/fanfiction-enhancements/master/fanfiction-enhancements.user.js
+// @updateURL    https://nekicat.github.io/fanfiction-enhancements/latest/fanfiction-enhancements.meta.js
+// @downloadURL  https://nekicat.github.io/fanfiction-enhancements/latest/fanfiction-enhancements.user.js
 // @require      https://raw.githubusercontent.com/taylorhakes/promise-polyfill/master/promise.min.js
 // @match        *://www.fanfiction.net/*
-// @grant        GM_getValue
-// @grant        GM_setValue
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
