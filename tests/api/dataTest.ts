55743ca320346ab6b7e8094e413c61cffa9a834c
Arne 'TigeR' Linck <a.linck@onlinehome.de>
2018-04-25 11:02:54 +0200

    Added data cache to manage reads, follows, favs...

--- tests/api/dataTest.ts
+++ tests/api/dataTest.ts
@@ -3,10 +3,27 @@ import { JSDOM } from "jsdom";
 import * as ko from "knockout";
 import * as sinon from "sinon";
 
+import { cache, Cache } from "../../src/util/cache";
 import { Chapter, Story } from "../../src/api/data";
 
 describe("Data Objects", function() {
+	before(function() {
+		global["cache"] = new Cache();
+	});
+
 	describe("Story", function() {
+		let isFollowed;
+		let setFollowed;
+		let isFavorited;
+		let setFavorited;
+
+		beforeEach(function() {
+			cache.alerts.isFollowed = isFollowed = sinon.stub();
+			cache.alerts.setFollowed = setFollowed = sinon.spy();
+			cache.alerts.isFavorited = isFavorited = sinon.stub();
+			cache.alerts.setFavorited = setFavorited = sinon.spy();
+		});
+
 		it("should report read if all chapters are read", function() {
 			const a = { read: ko.observable(true) };
 			const b = { read: ko.observable(true) };
@@ -68,57 +85,91 @@ describe("Data Objects", function() {
 			assert.isFalse(a.read());
 			assert.isFalse(b.read());
 		});
+
+		it("should retrieve follow value via cache", function() {
+			isFollowed.returns(true);
+
+			const sut = new Story(0, "", {
+				id: 0,
+				name: "",
+				profileUrl: "",
+				avatarUrl: "",
+			}, "", [{ read: ko.observable(false) }] as any, {});
+
+			sinon.assert.calledOnce(isFollowed);
+			sinon.assert.notCalled(setFollowed);
+		});
+
+		it("should set follow value via cache", function() {
+			const sut = new Story(0, "", {
+				id: 0,
+				name: "",
+				profileUrl: "",
+				avatarUrl: "",
+			}, "", [{ read: ko.observable(false) }] as any, {});
+
+			sut.follow(true);
+
+			sinon.assert.calledOnce(isFollowed);
+			sinon.assert.calledWith(setFollowed, sut);
+		});
+
+		it("should retrieve favorite value via cache", function() {
+			isFavorited.returns(true);
+
+			const sut = new Story(0, "", {
+				id: 0,
+				name: "",
+				profileUrl: "",
+				avatarUrl: "",
+			}, "", [{ read: ko.observable(false) }] as any, {});
+
+			sinon.assert.calledOnce(isFavorited);
+			sinon.assert.notCalled(setFavorited);
+		});
+
+		it("should set favorite value via cache", function() {
+			const sut = new Story(0, "", {
+				id: 0,
+				name: "",
+				profileUrl: "",
+				avatarUrl: "",
+			}, "", [{ read: ko.observable(false) }] as any, {});
+
+			sut.favorite(true);
+
+			sinon.assert.calledOnce(isFavorited);
+			sinon.assert.calledWith(setFavorited, sut);
+		});
 	});
 
 	describe("Chapter", function() {
-		let gmGetValue;
-		let gmSetValue;
-		let gmDeleteValue;
+		let isRead;
+		let setRead;
 
 		beforeEach(function() {
-			global["GM_getValue"] = gmGetValue = sinon.stub();
-			global["GM_setValue"] = gmSetValue = sinon.spy();
-			global["GM_deleteValue"] = gmDeleteValue = sinon.spy();
+			cache.read.isRead = isRead = sinon.stub();
+			cache.read.setRead = setRead = sinon.spy();
 		});
 
-		it("should return false from read by default", function() {
-			const sut = new Chapter(123, 1, "chapter");
-
-			assert.isFalse(sut.read());
-			sinon.assert.calledOnce(gmGetValue);
-			sinon.assert.notCalled(gmSetValue);
-			sinon.assert.notCalled(gmDeleteValue);
-		});
-
-		it("should retrieve read value via GM", function() {
-			gmGetValue.returns(true);
+		it("should retrieve read value via cache", function() {
+			isRead.returns(true);
 
 			const sut = new Chapter(123, 1, "chapter");
 
 			assert.isTrue(sut.read());
-			sinon.assert.calledOnce(gmGetValue);
-			sinon.assert.notCalled(gmSetValue);
-			sinon.assert.notCalled(gmDeleteValue);
+			sinon.assert.calledOnce(isRead);
+			sinon.assert.notCalled(setRead);
 		});
 
-		it("should set read value via GM", function() {
+		it("should set read value via cache", function() {
+			isRead.returns(false);
+
 			const sut = new Chapter(123, 1, "chapter");
 			sut.read(true);
 
-			sinon.assert.calledOnce(gmGetValue);
-			sinon.assert.calledWith(gmSetValue, "ffe-story-123-chapter-1-read", true);
-			sinon.assert.notCalled(gmDeleteValue);
-		});
-
-		it("should delete read value via GM", function() {
-			gmGetValue.returns(true);
-
-			const sut = new Chapter(123, 1, "chapter");
-			sut.read(false);
-
-			sinon.assert.calledOnce(gmGetValue);
-			sinon.assert.notCalled(gmSetValue);
-			sinon.assert.calledWith(gmDeleteValue, "ffe-story-123-chapter-1-read");
+			sinon.assert.calledOnce(isRead);
+			sinon.assert.calledWith(setRead, sut);
 		});
 	});
 });
